f7f4d0f610c6b782f3b79a84493b2b9fc9665432
Padam Bhandari <Padam.Bhandari@datascan.com>
2017-02-23 15:55:15 -0600

    null check and remove from list

---
+++ RemoveFromList/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveFromList
+{
+    using System.Dynamic;
+
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public string name { get; set; }
+
+        public string email { get; set; }
+
+        public DateTime date { get; set; }
+
+
+    
+    }
+}
---
+++ RemoveFromList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveFromList
+{
+    using System.Security.Cryptography.X509Certificates;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Person>mylist =new List<Person>();
+            mylist.Add(new Person() {ID = 1,name = "Padam",date = Convert.ToDateTime("05/27/2017"),email = "padam10@yahoo.com"});
+            mylist.Add(new Person() {ID = 2,name = "Adam",date = Convert.ToDateTime("04/27/2017")});
+            mylist.Add(new Person() {ID = 3,name = "samy",date = Convert.ToDateTime("05/27/2017"),email = "pdfsf10@yahoo.com"});
+            mylist.Add(new Person() {ID = 4,name = "test",date = Convert.ToDateTime("05/27/2017"),email = ""});
+
+            foreach (var person in mylist)
+            {
+                if (string.IsNullOrEmpty(person.email))
+                {
+                    Console.WriteLine("no email address {0}",person.name);
+                }
+            }
+
+            mylist.RemoveAll(x =>string.IsNullOrEmpty(x.email));
+
+            foreach (var person in mylist)
+            {
+                    Console.WriteLine("list of emp after null check {0}", person.name);
+                
+            }
+
+            Console.Read();
+        }
+    }
+}
